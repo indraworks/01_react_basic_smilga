94a828cb5027ff3bed3b11f5de5e85e90c055e38
indraworks <indra.works2020@gmail.com>
2022-04-10 14:19:47 +0700

    01x penjelasan jx rules

--- src/index.js
+++ src/index.js
@@ -3,26 +3,32 @@ import { render } from "@testing-library/react"
 import React from "react"
 import ReactDOM from "react-dom"
 
+//RULE JSC yg perlu diingat
+//1 smua harus 1 kluraan div ,dan haay ada 1 kembalian  kalau banyak div atau kalang tag
+//kembalianjika banya bisa dipakai React.Fragment atau cukup tulis <> </> sbgai kalang penutup
+
+//utk cssnya didalam jsx maka dari class berubah dgn className="bla bla"
+//utk atribute css penggunaanya camelClase
+//pentup tag boleh sebarang div.article/section /Fragment sprti diatas
+//utk format bisa begni
+/*
+ return <div> </div>  //ini harus ada diseblaj kana tag utk tag pmbuka 
+                      //kalau tidak dia error
+ atau return pakai kurung /parentheses
+ return (
+   <div> </div>
+ )
+
+
+*/
+
 //ini component yg dikembalikan
 function MyComponent() {
-  // return (<div>
-  //          <h1>hello ini cara pakai React.createElement</h1>
-  // </div>)
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "hello ini dgn create element!")
+  return (
+    <div>
+      <h1>hello ini cara pakai React.createElement</h1>
+    </div>
   )
 }
 
 ReactDOM.render(<MyComponent />, document.getElementById("root"))
-
-/*
-jadi kalau mau pakai reac.element 
-caranya rumus sama dia adalah tag,{props},childer isi yg ada di element
-nah mislakan ada 2 kalang maka yg pertama harus disebut 
-nah utk kalang kedua ditulis di paling belakanag dekat kurung penutup
-esbgai crete element yg baru begitu seterunya inicuman asal mulanya saja
-
-
-*/
