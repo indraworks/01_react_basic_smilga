24a6be86dccdff57c23e80fe5437efc1ead8cd52
indraworks <indra.works2020@gmail.com>
2022-04-12 09:01:51 +0700

    desctruting props -1

--- src/index.js
+++ src/index.js
@@ -4,28 +4,32 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
-/*Tentang Props
-kita buat jadi 1 dan kita mengerti apa itu props 
-yaitu sama dgn parameter tapi props itu bentu dirinya sebgai paramerer object
-nnti kita lihat nah skgn kita refactor ini jadi 1 masing2
-jadi kalau pada books function itu parameter props di print atau di console.log
-maka hasilnya adalah sbuah object { }
-nah sekarang compinent book ini akan kita buat atau isidari Booklis 
-masing2 kita lewatkan paraneter sbb:
-jika kita isi salah 1  <Book title =
- )
-
-*/
-
 const Booklist = () => {
-  const Title = "Hello Indra"
-  const Image =
-    "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg"
-  const Author = "Cindy Patikasari"
+  //kita refacor data2 jadi nama book dgn object propertes didalamnya
+  const firstBook = {
+    Title: "Mrah Putih Indonesia",
+    Image:
+      "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg",
+    Author: "Cindy Patikasari",
+  }
+  const secondBook = {
+    Title: "Penjual Jamu",
+    Image: "https://m.media-amazon.com/images/I/81vpsIs58WL._AC_SY200_.jpg",
+    Author: "BronxBronx",
+  }
 
   return (
     <section className='booklist'>
-      <Book title={Title} image={Image} author={Author} />
+      <Book
+        title={firstBook.Title}
+        image={firstBook.Image}
+        author={firstBook.Author}
+      />
+      <Book
+        title={secondBook.Title}
+        image={secondBook.Image}
+        author={secondBook.Author}
+      />
     </section>
   )
 }
@@ -33,9 +37,52 @@ const Booklist = () => {
 //props imi adalah parameter yg bertype data object
 //isi element obejctnya terserah bisa aray,bisa variable biasa etc!
 const Book = (props) => {
-  console.log(props)
-  /*
-    hasil console.log(props)
+  const { title, image, author } = props //descturcting utk jsx
+  //kita panggil langsung property keynya /element keynya
+  console.log("props =", props)
+
+  return (
+    <article className='book'>
+      <img src={image} alt='bookFavorite01' />
+      <h1>{title}</h1>
+      <h4 className='myauthor'> {author}</h4>
+    </article>
+  )
+}
+
+ReactDOM.render(<Booklist />, document.getElementById("root"))
+
+/*
+ const Book = (props) => {
+  const { title, image, author } = props //descturcting utk jsx
+  
+  console.log("props =", props)
+   hasil llog tetap sama jadi propnya berupa object dgn element2 ygpunya ilai 
+   
+
+
+
+
+*/
+
+/*
+2) stlah kita tahu bahwa props adalah property parameter yg berupa object
+dimana ada banyak atau boleh element2 dibawahnya maka kita mau test kalau kita destructing  gimana 
+diatas props berisi ketika dilwatkan ke Book 
+ <Book title={Title} image={Image} author={Author} />
+ maka props = { author: "Cindy Patikasari"
+      image: "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg"
+      title: "Hello Indra" }
+ nah kita panggil di dalam jsx  props.title maka hasil adalah "hello Indra" 
+ nah kita mau skgn destucting tanpa mesti harus panggil props depanya di depan diaas retun kitaberi 
+  key kita jadikan desctructing biar langsung panggil isinya 
+ const {author,image,title} = props
+      
+
+*/
+
+/*
+   1) hasil console.log(props)
        {author: "Cindy Patikasari"
       image: "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg"
       title: "Hello Indra" }
@@ -57,14 +104,3 @@ const Book = (props) => {
 
      
       */
-
-  return (
-    <article className='book'>
-      <img src={props.image} alt='bookFavorite01' />
-      <h1>{props.title}</h1>
-      <h4> {props.author}</h4>
-    </article>
-  )
-}
-
-ReactDOM.render(<Booklist />, document.getElementById("root"))
