96206b73739f5d91350cfcd7fe430d9cff88c0e1
indraworks <indra.works2020@gmail.com>
2022-04-13 07:49:33 +0700

    30.spread dan props correct way

--- src/index.js
+++ src/index.js
@@ -17,15 +17,24 @@ jika array biasa kita masukan ke props dgn cara map
 const Names = ["indra", "summy", "chica", "husnul"]
 const bookdata = [
   {
+    Id: 1,
     Title: "Mrah Putih Indonesia",
     Image:
       "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg",
     Author: "Cindy Patikasari",
   },
   {
+    Id: 2,
     Title: "Penjual Jamu",
     Image: "https://m.media-amazon.com/images/I/81vpsIs58WL._AC_SY200_.jpg",
     Author: "BronxBronx",
+  },
+  {
+    Id: 3,
+    Title: "Minyak goreng langka ",
+    Image:
+      "https://images-na.ssl-images-amazon.com/images/I/51wfzW0QnXL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    Author: "Indra Suryawan ",
   },
 ]
 
@@ -34,35 +43,55 @@ const Booklist = () => {
     <section className='booklist'>
       {/* memasukan BookDAta
        */}
-      {bookdata.map((book) => {
+
+      {bookdata.map((item) => {
+        //const { Title, Image, Author } = item
         return (
-          <>
-            <h1>{book.Title}</h1>
-            <h1>{book.Author}</h1>
-            <h1>{book.Image}</h1>
-          </>
+          // <Book title={book.Title} image={book.Image} author={book.Author} />
+          //<Book key={item.Id} book={item} />
+          <Book key={item.Id} {...item} />
         )
       })}
     </section>
   )
 }
 
-const Book = (props) => {
-  const { title, image, author, children } = props
+const Book = ({ Title, Image, Author }) => {
+  //const { Title, Image, Author } = props.book
+  //nama book di props.book berasal dari variable params book dari: book={item} harus sama ini namanya!
 
-  console.log("props =", props)
   return (
     <article className='book'>
-      <img src={image} alt='bookFavorite01' />
+      <img src={Image} alt='bookFavorite01' />
 
-      <h1 className='mytitle'>{title}</h1>
-      <h6>{children}</h6>
-      <h4 className='myauthor'> {author}</h4>
+      <h1 className='mytitle'>{Title}</h1>
+
+      <h4 className='myauthor'> {Author}</h4>
     </article>
   )
 }
 
 ReactDOM.render(<Booklist />, document.getElementById("root"))
+
+/*Pencarahan utk spread operator  dan propsnya d Book function
+nah diaas kita masukan props pada <Book /> compoent dgn 
+ <Book key={item.Id} book={item} /> dengan Object nah ini  book ={item} kita ganti saja
+ kita spread isi array diatas jadi 1 item kan isinya {id:1,Title:'bla',Author:'bla',Image:'bla}
+ nah daripada saya tulis satu2 jadi object saya bisalangsung kluarkan dari object dgn peritnah 
+  book={item} saya ganti jadi {...item }
+  nah di bawah di Book Function propsnya nama mesti saama
+  const Book =({Title,Author,Image})=> {
+
+  }
+//dan saaya udah gak perlu lagi pake destruction 
+//const {Title,Author,Image} = props.item udah gake perlu lagi
+
+lengkap linknya disini baik utk objec dan araray oeprasi spread operator:
+https://www.w3schools.com/react/react_es6_spread.asp
+jadi 
+
+
+*/
 
 /* latihan 1 
 pada contoh pertama saya merender utk smua isi dari array object yg ada di boookData
@@ -113,13 +142,74 @@ const Booklist = () => {
 /*
 pada latiha3 saya mau pakai Book component utk render tampilan 
 bookdata array object,
-nah ini dia pointnya karena prop itu meupaka objecc dgn element  props = {element} 
+nah ini dia pointnya karena prop itu meupaka objecc dgn element  props = {element} bookdata data
 utk sekarang kita tahu props ={title,img,author } 
 
+const Booklist = () => {
+  return (
+    <section className='booklist'>
+      
+      {bookdata.map((book) => {
+        return (
+          <Book title={book.Title} image={book.Image} author={book.Author} />
+        )
+      })}
+    </section>
+  )
+}
+
+nah hasilnya akan tampak speti yg seblumnya ada card cardnya ada 2 card 
+dan cara isi insert propnya ke Book kita jalamka dgn isikan element2 object property kedalam 
+paramter diatas ..
+ <Book title={book.Title} image={book.Image} author={book.Author} />
+ nah kita gak perlu tulis semua seperti diatas title={book.Title} image={book.Image} author={book.Author}
+ nahkita bisa refactor kode diatas yatu diwakil dgn 1variable obhject 
+ mis:
+ <Book book= {book } <--nah book dikurung kurwal ini adalah object
 
 
+*/
 
+/*
+latihan 4:
+nah sekarang kita akan pakai desctruction jadi kan kita tahu bawah 
+element obejct yg masuk prop sebelumnya nulis 1 1 sperti dibawah ini :
+kita pasing paramter ke function Box dengan inject props melalui params
+ title,image,author 
+ nah mauna kit adalah cukup dng 1 buath variable yg mewakili  3 element object title,image,author
+  old :    
+<Book title={book.Title} image={book.Image} author={book.Author} />
+  baru: <Book book={item}  /> nah kita tahu element item itu mewakili per index dari bookdata  per-index 
+  yg mana di looping dikalang map bisa liat kodenya diatas
+  yaitu :bookdata.map(item => {
+    return <Book book={item}
+  })
+  item sendiri isinya :[{element},{element }]
+  item[0]=  {
+    Title: "Mrah Putih Indonesia",
+    Image:
+      "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg",
+    Author: "Cindy Patikasari",
+  }
 
+  item[1] ={
+    Title: "Penjual Jamu",
+    Image: "https://m.media-amazon.com/images/I/81vpsIs58WL._AC_SY200_.jpg",
+    Author: "BronxBronx",
+  }
+jadi diaas item adalah paramter yg pasing ke map function 
+nah supaya match dgn Book yg merupakan functoon yg mana nilai Title,Image,Author masuk ke paramya 
+/props yg merupakan object utk itu kita kenalkan pada func Book ini 
+nasepei kita ketahu yg namaya props adalah objec dimasna dala case ini element 
+yg masuk param pada function Book adalah {Title,Image,Authot}
+nah disini kita buat desctuctionya di fucntion Book tiga element object diatas 
+akan diwakili dgn variable destructon bernama book 
+const {Title,Image,Author } = props.book 
 
+nah kita samakan sesua dgn ygada di booklist parameter yg  masuk kepropsnya bernama book 
+<Book book={item} />   //liat book yg sebelah kiri  nah dia itu 
+equal sama dgn yg ada di Book fucntion yaitu :
 
+const {Title,Image,Author } = props.book  ~ jadi harus sama dng  book yg di loop map : <Book book={item>}
+semoga ini bisa mencrahkan !
 */
---
+++ src/test_spread.js
@@ -0,0 +1,13 @@
+const bookdata = [
+  {
+    Title: "Mrah Putih Indonesia",
+    Image:
+      "https://images-na.ssl-images-amazon.com/images/I/81Jg8HF-N1L._AC_UL127_SR127,127_.jpg",
+    Author: "Cindy Patikasari",
+  },
+  {
+    Title: "Penjual Jamu",
+    Image: "https://m.media-amazon.com/images/I/81vpsIs58WL._AC_SY200_.jpg",
+    Author: "BronxBronx",
+  },
+]
